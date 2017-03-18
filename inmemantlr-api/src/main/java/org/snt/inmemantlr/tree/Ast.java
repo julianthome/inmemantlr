6692139cf0ef2401deaa66758f683862577f5bd7
Julian Thome <julian.thome@uni.lu>
2017-03-18 12:34:54 +0100

    header

--- inmemantlr-api/src/main/java/org/snt/inmemantlr/tree/Ast.java
+++ inmemantlr-api/src/main/java/org/snt/inmemantlr/tree/Ast.java
@@ -1,20 +1,20 @@
 /**
  * Inmemantlr - In memory compiler for Antlr 4
- * <p>
+ *
  * The MIT License (MIT)
- * <p>
+ *
  * Copyright (c) 2016 Julian Thome <julian.thome.de@gmail.com>
- * <p>
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
  * the Software without restriction, including without limitation the rights to
  * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
  * of the Software, and to permit persons to whom the Software is furnished to do
  * so, subject to the following conditions:
- * <p>
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * <p>
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -48,7 +48,7 @@ public class Ast {
 
     /**
      * constructor
-     * <p>
+     *
      * create a new abstract syntax tree
      *
      * @param nt    name of root non-terminal node
@@ -61,7 +61,7 @@ public class Ast {
 
     /**
      * constructor
-     * <p>
+     *
      * copy constructor
      *
      * @param tree tree to be duplicated
@@ -73,7 +73,7 @@ public class Ast {
 
     /**
      * constructor
-     * <p>
+     *
      *
      * @param nod root node
      */
